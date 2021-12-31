4143a43d520cf09b6fcd40ae329a21a2cbb5e25f
Matthew <60657144+mjguzman12@users.noreply.github.com>
2021-12-31 14:48:39 -0500

    Update 2. Graphics and Tables.R

--- Code/2. Graphics and Tables.R
+++ Code/2. Graphics and Tables.R
@@ -7,7 +7,7 @@
 rm(list = ls())
 
 ## Set Analysis directory
-setwd("/Users/matthew/Desktop/Research/On Campus Housing/Analysis - GitHub")
+setwd("[SET DIRECTORY]/Analysis")
 
 source("Code/IPEDS Student Services Percentage Graph.R")
 source("Code/IPEDS Summary Table Creation.R")
